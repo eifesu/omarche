67ce866d4edc4734416075956ccc8eabee4bed70
Chris YESSO <yessochrisa@gmail.com>
2025-03-20 11:52:04 +0000

    Added order-handler and refactor

--- api/prisma/middleware/index.ts
+++ api/prisma/middleware/index.ts
@@ -1,9 +1,9 @@
-import { notifyOrderCreated } from "@/utils/mail";
 import { Prisma } from "@prisma/client";
+import { handleOrderCreated } from "../handlers/order-handlers";
 
 const onOrderCreated: Prisma.Middleware = async (params, next) => {
   if (params.action === "create" && params.model === "Order") {
-    notifyOrderCreated();
+    await handleOrderCreated(params.args);
   }
   return next(params);
 };
